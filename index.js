f8518c6716df5eb2b11854650265f6a03b7cab92
cogwizzle <joseph.fehrman@gmail.com>
2021-11-16 20:30:48 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,119 @@
+const generateVariablesAndRegularExpression = (path) => {
+  const regex = /{[a-zA-Z]*}/g;
+  // Get variable names.
+  const variableNames = [...path.matchAll(regex)].map((match) => {
+    return match[0].replace(/[{|}]/g, '');
+  });
+  // Get matcher.
+  const regularExpression = new RegExp(
+    path.replaceAll(regex, `([a-zA-Z1-9\\-]*)`).replaceAll(/\//g, '/')
+  );
+  return {
+    variableNames,
+    regex: regularExpression,
+  };
+};
+
+const getVariables = (path, { variables, regex }) => {
+  const variableValues = [...path.match(regex)].filter((value) => value !== '');
+  variableValues.shift();
+  return variableValues.map((value, index) => ({
+    name: variables[index],
+    value,
+  }));
+};
+
+const createRouteIndex = (route) => {
+  const { variableNames, regex } = generateVariablesAndRegularExpression(
+    route.path
+  );
+  return {
+    ...route,
+    variables: variableNames,
+    regex,
+  };
+};
+
+const createRouteIndexes = (routes) => routes.map(createRouteIndex);
+
+export class RouterElement extends HTMLElement {
+  constructor() {
+    super();
+    if (this.constructor == RouterElement) {
+      throw new Error("Abstract classes can't be instantiated.");
+    }
+  }
+
+  indexRoutes() {
+    if (!this.routes)
+      throw new Error(
+        'Router element needs routes defined in the connected callback.'
+      );
+    this._indexedRoutes = createRouteIndexes(
+      this.routes.sort((a, b) => {
+        return a.path.length < b.path.length ? 1 : -1;
+      })
+    );
+  }
+
+  get indexedRoutes() {
+    return this._indexedRoutes;
+  }
+
+  onNavigate() {
+    const path = window.location.pathname;
+
+    if (path !== this.currentPath) {
+      this.currentPath = path;
+      this.render();
+    }
+  }
+
+  notFound() {
+    this.innerHTML = `
+      <h1>Unable to find page</h1>
+      <div>
+        I'm sorry the page you're looking for was unable to be found.
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    this.indexRoutes();
+    this.currentPath = window.location.pathname;
+    this.render();
+    window.addEventListener('navigate', this.onNavigate.bind(this));
+  }
+
+  disconnectedCallback() {
+    window.removeEventListener('navigate', this.onNavigate.bind(this));
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.indexRoutes();
+      this.render();
+    }
+  }
+
+  async render() {
+    const route = this.indexedRoutes.find((indexedRoute) =>
+      indexedRoute.regex.test(this.currentPath)
+    );
+    if (!route || (route.path === '' && this.currentPath !== '')) {
+      this.notFound();
+      return;
+    }
+    const variables = getVariables(this.currentPath, route);
+    const template = document.createElement('template');
+    try {
+      template.innerHTML = await route.go({
+        variables,
+      });
+      this.innerHTML = '';
+      this.appendChild(template.content.cloneNode(true));
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,117 @@
+# ez-client-router
+
+Easy to use client router custom element. No frameworks, no polyfills, no dependencies.
+
+## Installation
+
+```sh
+npm i --save ez-client-router
+```
+
+## Basic Usage
+
+You have to create a router element by extending the RouterElement base class. You can then insert your routes into the router element and it will take care fo the rest.
+
+my-custom-router.js
+
+```JavaScript
+import { RouterElement } from 'ez-client-router';
+import { HelloWorld } from './src/pages/hello-world.js';
+
+// Optionally declare the component if it doesn't exist.
+if (customElements.get('my-hello-world') === undefined) {
+  customElements.define('my-hello-world', HelloWorld);
+}
+
+export class MyCustomRouter extends RouterElement {
+  constructor() {
+    super():
+  }
+
+  connectedCallback() {
+    // Create your routes.
+    this.routes = [
+      {
+        "path": "", // Path to the base route.
+        "go": () => `<my-hello-world></my-hello-world>` // Template you wish to render.
+      }
+    ];
+    super.connectedCallback(); // Run the connected callback of the RouterElement.
+  }
+}
+```
+
+index.html
+
+```HTML
+<script type="module">
+import { MyCustomRouter } from './src/components/my-custom-router.js';
+
+customElements.define('my-custom-router', MyCustomRouter);
+</script>
+<my-custom-router></my-custom-router>
+```
+
+## Features
+
+### Variables in the path.
+
+ez-client-router supports variables in the path. You can declare the variables in the path surrounding them in curly braces. Here is an example:
+
+```
+/blog/{postId}
+```
+
+The variable will be applied to the component at that path as an attribute.
+This route:
+
+```JavaScript
+{
+  path: '/blog/{post-id}',
+  go: ({ variables }) => {
+    return `
+      <my-blog-post post-id="${variables['post-id']}"></my-blog-post>
+    `;
+  }
+}
+```
+
+With this URL:
+
+```
+/post/123
+```
+
+Will render:
+
+```HTML
+<my-blog-post post-id="123"></my-blog-post>
+```
+
+Path variables can only be composed of alphanumeric characters and hyphens.
+
+### Path not found
+
+When you try to reach a route that is not defined, the router will render a notFound message. You can customize this message by creating your own custom notFound function and setting this.innerHTML to the template you desire.
+
+```JavaScript
+class MyRouter extends RouterElement {
+  constructor() {
+    super();
+  }
+
+  notFound() {
+    this.innerHTML = `Oh no! We couldn't find that page.`;
+  }
+
+  connectedCallback() {
+    this.routes = [
+      {
+        path: '/',
+        go: () => `<my-home-page></my-home-page>`
+      }
+    ];
+    super.connectedCallback();
+  }
+}
+```
