ddaea46b40a15cbd6f9946f8b9315a18775f4dff
Joe Fehrman <joseph.fehrman@gmail.com>
2025-12-10 21:33:45 -0500

    feat: Adding type safety to the ez-client-router

--- index.mjs
+++ index.mjs
@@ -1,12 +1,34 @@
+/**
+ * @typedef {object} Route
+ * @property {string} path
+ * @property {string} title
+ * @property {({ variables }: { variables: {name: string, value: string }[]}) => Promise<string>} go
+ */
+
+/**
+ * @typedef {object} IndexedRoute
+ * @property {string} path
+ * @property {string} title
+ * @property {({ variables }: { variables: { name: string, value: string }[]}) => Promise<string>} go
+ * @property {string[]} variables
+ * @property {RegExp} regex
+ */
+
+/**
+ * Generates the variables and regular expression for a given path.
+ *
+ * @param {string} path The path to generate the variables and regular expression for.
+ * @returns {{variableNames: string[], regex: RegExp}}
+ */
 const generateVariablesAndRegularExpression = (path) => {
   const regex = /{[a-zA-Z]*}/g;
   // Get variable names.
   const variableNames = [...path.matchAll(regex)].map((match) => {
-    return match[0].replace(/[{|}]/g, '');
+    return match[0].replace(/[{|}]/g, "");
   });
   // Get matcher.
   const regularExpression = new RegExp(
-    path.replaceAll(regex, `([a-zA-Z1-9\\-]*)`).replaceAll(/\//g, '/')
+    path.replaceAll(regex, `([a-zA-Z1-9\\-]*)`).replaceAll(/\//g, "/"),
   );
   return {
     variableNames,
@@ -14,8 +36,15 @@ const generateVariablesAndRegularExpression = (path) => {
   };
 };
 
+/**
+ * Gets the variables from a given path.
+ *
+ * @param {string} path The path to get the variables from.
+ * @param {{ variables: string[], regex: RegExp}} route The route to get the variables from.
+ * @returns {{name: string, value: string}[]}
+ */
 const getVariables = (path, { variables, regex }) => {
-  const variableValues = [...path.match(regex)].filter((value) => value !== '');
+  const variableValues = path.match(regex) || [];
   variableValues.shift();
   return variableValues.map((value, index) => ({
     name: variables[index],
@@ -23,9 +52,15 @@ const getVariables = (path, { variables, regex }) => {
   }));
 };
 
+/**
+ * Creates a route index.
+ *
+ * @param {Route} route The route to create the index for.
+ * @returns {IndexedRoute}
+ */
 const createRouteIndex = (route) => {
   const { variableNames, regex } = generateVariablesAndRegularExpression(
-    route.path
+    route.path,
   );
   return {
     ...route,
@@ -34,9 +69,17 @@ const createRouteIndex = (route) => {
   };
 };
 
+/**
+ * Creates the route indexes.
+ *
+ * @param {Route[]} routes The routes to create the indexes for.
+ * @returns {IndexedRoute[]}
+ */
 const createRouteIndexes = (routes) => routes.map(createRouteIndex);
 
 export class RouterElement extends HTMLElement {
+  /** @type {Route[]} */
+  routes = [];
   constructor() {
     super();
     if (this.constructor == RouterElement) {
@@ -47,12 +90,12 @@ export class RouterElement extends HTMLElement {
   indexRoutes() {
     if (!this.routes)
       throw new Error(
-        'Router element needs routes defined in the connected callback.'
+        "Router element needs routes defined in the connected callback.",
       );
     this._indexedRoutes = createRouteIndexes(
       this.routes.sort((a, b) => {
         return a.path.length < b.path.length ? 1 : -1;
-      })
+      }),
     );
   }
 
@@ -82,11 +125,11 @@ export class RouterElement extends HTMLElement {
     this.indexRoutes();
     this.currentPath = window.location.pathname;
     this.render();
-    window.addEventListener('navigate', this.onNavigate.bind(this));
+    window.addEventListener("navigate", this.onNavigate.bind(this));
   }
 
   disconnectedCallback() {
-    window.removeEventListener('navigate', this.onNavigate.bind(this));
+    window.removeEventListener("navigate", this.onNavigate.bind(this));
   }
 
   attributeChangedCallback(name, oldValue, newValue) {
@@ -97,23 +140,23 @@ export class RouterElement extends HTMLElement {
   }
 
   async render() {
-    const route = this.indexedRoutes.find((indexedRoute) =>
-      indexedRoute.regex.test(this.currentPath)
+    const route = this.indexedRoutes?.find((indexedRoute) =>
+      indexedRoute.regex.test(this.currentPath),
     );
     if (
       !route ||
-      (route.path === '' && this.currentPath !== '' && this.currentPath !== '/')
+      (route.path === "" && this.currentPath !== "" && this.currentPath !== "/")
     ) {
       this.notFound();
       return;
     }
     const variables = getVariables(this.currentPath, route);
-    const template = document.createElement('template');
+    const template = document.createElement("template");
     try {
       template.innerHTML = await route.go({
         variables,
       });
-      this.innerHTML = '';
+      this.innerHTML = "";
       this.appendChild(template.content.cloneNode(true));
     } catch (e) {
       throw new Error(e);
